96eea954b45e6b5ca4eb4af90f11207220c365c0
18012274 <18012274@d020m04.istic.univ-rennes1.fr>
2021-02-23 12:17:47 +0100

    Debut de correction Tolerance

--- src/Implementations/Tolerance.scala
+++ src/Implementations/Tolerance.scala
@@ -8,16 +8,14 @@ object Tolerance{
  // factory
   
 	def hamming(s1: String, s2: String): Int = {
-			if (s1.length != s2.length)
-				return Math.abs(s2.length-s1.length())
-				(s1.toList).zip(s2.toList)
-				.filter(current => current._1 != current._2)
-				.length
+			if (s1.length != s2.length) Math.abs(s2.length-s1.length())
+			else s1.zip(s2).count(pair => pair._1 != pair._2)
+		  //else(s1.toList).zip(s2.toList).filter(current => current._1 != current._2).length
 	} 
 	//hamming("rara","xx")
 	
 	def correct(s1: String, s2: String):Boolean = {
-	  if((hamming(s1,s2)== 0) || (hamming(s1,s2)== 1)) return true else false 
+	  if((hamming(s1,s2)<= 1)) return true else false 
 	} 
 	//correct("rara","rare") *
 } 
