a7a8444d37fd2c19e71579e7c122fe6a3b77b156
mbersanoukae <mbersanoukae@d020m07.istic.univ-rennes1.fr>
2021-02-23 10:47:00 +0100

    methode listKeyWorks implemente

--- src/Implementations/BSDImpl.scala
+++ src/Implementations/BSDImpl.scala
@@ -1,13 +1,18 @@
 package Implementations
+import scala.io.Source
 
 object BSDImpl extends BaseDeDonnée{
   
-  def respond(l:List[String]):List[(String,String)]={
-    
-  }
+  def respond(l:List[String]):List[(String,String)]= ???
   
   def listKeyWords:List[List[String]]={
-    
+      var res :List[List[String]] = List()
+      val txt =Source.fromFile("doc/DonneesInitiales.txt").getLines
+      for(x<-txt){
+        val i = x.indexOf(";")
+        res = res :+ List(x.slice(0, i),x.slice(i+1,x.length))
+      }
+      res
   }
   
 }
