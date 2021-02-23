4248a91863a7e8563a8ddcab323484e4a5e9ae89
mbersanoukae <mbersanoukae@d020m07.istic.univ-rennes1.fr>
2021-02-23 11:16:58 +0100

    Ajout commentaire

--- src/Implementations/BSDImpl.scala
+++ src/Implementations/BSDImpl.scala
@@ -14,7 +14,9 @@ object BSDImpl extends BaseDeDonnée{
     }
     res
   }
-
+  /** Extracteur de fichier
+   *  @param Aucun parametre, Assurez l'existence de "doc/DonneesInitiales.txt"
+   *  @return une list tel que :>> List(List(nom,adresse)) */
   def listKeyWords:List[List[String]]={
       var res :List[List[String]] = List()
       val txt =Source.fromFile("doc/DonneesInitiales.txt").getLines
