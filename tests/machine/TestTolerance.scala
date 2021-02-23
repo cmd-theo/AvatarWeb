1186e43235640beae79cc05aeb8c3d9a57c988a0
18012274 <18012274@d020m04.istic.univ-rennes1.fr>
2021-02-23 11:51:38 +0100

    Creations de tests pour Tolerance

---
+++ tests/machine/TestTolerance.scala
@@ -0,0 +1,25 @@
+package machine
+import org.junit.Test
+import org.junit.Assert._
+import Implementations.Tolerance
+
+class TestTolerance {
+  
+    @Test
+  def testTolHam{
+    assertEquals(1, Tolerance.hamming("Bonjour", "Bonjou"))
+    assertEquals(2, Tolerance.hamming("Bonjour", "Bonjo"))
+    assertEquals(1, Tolerance.hamming("Bonjour", "Bonjoure"))
+    assertEquals(7, Tolerance.hamming("Bonjour", ""))
+    assertEquals(0, Tolerance.hamming("Bonjour", "Bonjour"))
+  }
+  
+  @Test
+  def testTol{
+    assertEquals(true, Tolerance.correct("Bonjour", "Bonjou"))
+    assertEquals(false, Tolerance.correct("Bonjour", "Bonjo"))
+    assertEquals(true, Tolerance.correct("Bonjour", "Bonjoure"))
+    assertEquals(false, Tolerance.correct("Bonjour", ""))
+    assertEquals(true, Tolerance.correct("Bonjour", "Bonjour"))
+  }
+}
