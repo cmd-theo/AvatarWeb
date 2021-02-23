fcf5b9d305538cb4cf1a1e39550f0a1bdce12755
lbosse <lbosse@d020m08.istic.univ-rennes1.fr>
2021-02-23 10:03:05 +0100

    tarit bsd

--- src/machine/BaseDeDonnée.scala
+++ src/machine/BaseDeDonnée.scala
@@ -1,14 +1,13 @@
 package machine
 
 trait BaseDeDonnée {
-  
-  
-  /**
+  /**La fonction qui renvoit les pair de string associé à un mot clé
    * @param l La liste de mots que contient la requête
    * @result La liste indiquant si le mot est présent et la pair de string associé
    */
   def respond(l:List[String]):List[(Boolean,(String,String))]
-  /**
+  
+  /**La fonction qui renvois le contenu du fichier text de la base donnée
    * @result Le contenu du fichier txt conetant les données
    */
   def listKeyWords:List[List[String]]
