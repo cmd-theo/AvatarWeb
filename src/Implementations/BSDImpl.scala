7e8b54b37c5b40986b6483c8767fbbd1fa4d0a9d
lbosse <lbosse@d020m08.istic.univ-rennes1.fr>
2021-02-23 10:14:32 +0100

    BSDImpl

---
+++ src/Implementations/BSDImpl.scala
@@ -0,0 +1,13 @@
+package Implementations
+
+object BSDImpl extends BaseDeDonnée{
+  
+  def respond(l:List[String]):List[(String,String)]={
+    
+  }
+  
+  def listKeyWords:List[List[String]]={
+    
+  }
+  
+}
