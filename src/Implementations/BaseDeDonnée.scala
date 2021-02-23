08a82f2a9adffc2fa4f4e303b7935c82e0cea20b
lbosse <lbosse@d020m08.istic.univ-rennes1.fr>
2021-02-23 10:11:50 +0100

    correction

--- src/Implementations/BaseDeDonnée.scala
+++ src/Implementations/BaseDeDonnée.scala
@@ -3,9 +3,9 @@ package Implementations
 trait BaseDeDonnée {
   /**La fonction qui renvoit les pair de string associé à un mot clé
    * @param l La liste de mots que contient la requête
-   * @result La liste indiquant si le mot est présent et la pair de string associé
+   * @result La liste des pair de string associé
    */
-  def respond(l:List[String]):List[(Boolean,(String,String))]
+  def respond(l:List[String]):List[((String,String))]
   
   /**La fonction qui renvois le contenu du fichier text de la base donnée
    * @result Le contenu du fichier txt conetant les données
