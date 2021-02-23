e3d128edfa1665a3dfe084431818427fc73ee3e0
tboulaire <tboulaire@d020m09.istic.univ-rennes1.fr>
2021-02-23 10:24:22 +0100

    test_button

--- src/Implementations/UI.scala
+++ src/Implementations/UI.scala
@@ -1,7 +1,7 @@
 package Implementations
 
 import scala.swing._
-
+import event._
 class UI extends MainFrame {
   title = "GUI Program #1"
   preferredSize = new Dimension(320, 240)
@@ -13,5 +13,13 @@ object GuiProgramOne {
     val ui = new UI
     ui.visible = true
     println("End of main function")
+  }
+  class sendButton(lab: String) extends Button {
+  
+  // Ajout d'une reaction au clic sur le bouton
+  // c'est à dire copier le contenu du champ from dans le label de to.
+  reactions += {
+     case ButtonClicked(_)=> null
+  }
   }
 }
