9bd991966278c7ef7d6bb7b749f29655387c7418
lbosse <lbosse@d020m08.istic.univ-rennes1.fr>
2021-02-23 10:30:11 +0100

    update

--- src/Implementations/BSDImpl.scala
+++ src/Implementations/BSDImpl.scala
@@ -3,7 +3,16 @@ package Implementations
 object BSDImpl extends BaseDeDonnée{
   
   def respond(l:List[String]):List[(String,String)]={
+    val t = listKeyWords
+    var res:List[(String,String)]=List()
+    for(x<-l){
+     for(y<-t){
+        //if(y(0).contains(x)) res = res::(y(0),y(1))
+     }
+    }
     
+    
+    List(("",""))
   }
   
   def listKeyWords:List[List[String]]={
