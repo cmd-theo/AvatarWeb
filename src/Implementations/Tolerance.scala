848785b678a869fc477c921f002913d5f392b373
aaouad <aaouad@d020m03.istic.univ-rennes1.fr>
2021-02-23 10:46:04 +0100

    implementation des fonction sHamming et correct

--- src/Implementations/Tolerance.scala
+++ src/Implementations/Tolerance.scala
@@ -1,5 +1,23 @@
 package Implementations
 
-class Tolerance {
+object Tolerance{
   
-}
+/**
+ * Hamming
+ */
+ // factory
+  
+	def hamming(s1: String, s2: String): Int = {
+			if (s1.length != s2.length)
+				return Math.abs(s2.length-s1.length())
+				(s1.toList).zip(s2.toList)
+				.filter(current => current._1 != current._2)
+				.length
+	} 
+	//hamming("rara","xx")
+	
+	def correct(s1: String, s2: String):Boolean = {
+	  if((hamming(s1,s2)== 0) || (hamming(s1,s2)== 1)) return true else false 
+	}
+	//correct("rara","rare")
+} 
