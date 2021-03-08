2f50865fc8b462875f2229b405fe11db6710701a
Agayeva Gulizar <gulizar.agayeva@etudiant.univ-rennes1.fr>
2021-03-08 00:30:36 +0000

    Update AnalysePhraseImpl.scala

--- src/Implementations/AnalysePhraseImpl.scala
+++ src/Implementations/AnalysePhraseImpl.scala
@@ -8,6 +8,10 @@ object AnalysePhraseImpl extends AnalysePage {
   def hash(s:String) : List[String] = {
     s.split(" ").toList.distinct // retire les doublons de la liste également ! 
   }
+
+  def reinit = {
+    val langue = "FR"
+  }
   
   def contains(l:List[String]) : List[String] = ???
   
