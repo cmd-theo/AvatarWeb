81ed7a1be743bce243639a8377cfae9247c904ca
Agayeva Gulizar <gulizar.agayeva@etudiant.univ-rennes1.fr>
2021-03-09 08:36:45 +0000

    Update MachineImpl.scala

--- src/Implementations/MachineImpl.scala
+++ src/Implementations/MachineImpl.scala
@@ -6,13 +6,40 @@ object MachineImpl extends MachineDialogue{
   
   var analysePhrase = AnalysePhraseImpl
   
+  val repTNB : String = "L'adresse de Théâtre National de Bretagne est : 1, Rue Saint-Hélier"
+  val repGare : String = "L'adresse de Gare SNCF est : 19, Place de la Gare"
+  val repMairie : String = "L'adresse de Mairie de Rennes est : Place de la Mairie"
+	val repTheatre : String = "L'adresse de Théâtre La Paillette est : 2, Rue du Pré de Bris"
+	
+  val donnees : Map[String,String] = Map(("bonjour", "bonjour"),
+      ("salut", "salut"),
+      ("théâtre national de bretagne", repTNB),
+			("theatre national de bretagne", repTNB),
+			("théâtre de bretagne", repTNB),
+			("theatre de bretagne", repTNB),
+			("tnb", repTNB),
+			("gare", repGare),
+      ("sncf", repGare),
+      ("mairie", repMairie),
+      ("mairie de rennes", repMairie),
+      ("hôtel de ville", repMairie),
+      ("hotel de ville", repMairie),
+      ("paillette", repTheatre),
+			("théâtre la paillette", repTheatre),
+			("theatre la paillette", repTheatre),
+			("merci", "merci"),
+			("merci aurevoir", "merci aurevoir"),
+			("aurevoir", "aurevoir"))
+			
+
+  
   /**
    * Envoi d'une requête requete et recuperation de sa reponse
    * @param s la requete
    * @return reponse la liste de reponses
    */
   def ask(s:String):List[String]= {
-    var requete = s
+    val requete = s
     val reponse = analysePhrase.hash(requete)
     reponse
   }
@@ -22,7 +49,7 @@ object MachineImpl extends MachineDialogue{
    * Reinitilisation de l'avatar
    */
   def reinit= {
-    analysePhrase.reinit
+    
   }
   
   /**
