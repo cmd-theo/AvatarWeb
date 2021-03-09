4ead40fc768c0d5deade382ca21ca746e96727d9
15005748 <15005748@d020m06.istic.univ-rennes1.fr>
2021-03-09 11:33:01 +0100

    Ajout tests fonction contain.

--- tests/machine/TestAnalysePhrase.scala
+++ tests/machine/TestAnalysePhrase.scala
@@ -5,4 +5,13 @@ import Implementations.AnalysePhraseImpl
 
 class TestAnalysePhrase {
   
+  @Test
+  def testMotsClés{
+    val s1 = "Mairie de Rennes"
+    val s2 = "Bonjour, théâtre de Bretagne"
+    val s3 = "Théotre du Brtagne"
+    assertEquals (List ("Mairie", "Rennes"), AnalysePhraseImpl.contains(AnalysePhraseImpl.hash(s1)))
+    assertEquals (List ("Théâtre", "Bretagne"), AnalysePhraseImpl.contains(AnalysePhraseImpl.hash(s2)))
+    assertEquals (List ("Théâtre", "Bretagne"), AnalysePhraseImpl.contains(AnalysePhraseImpl.hash(s3)))
+  }
 }
