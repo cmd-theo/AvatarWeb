439a2fb0d54d84ae5e0a0cdef0e267df39208ac3
tboue <tboue@d020m10.istic.univ-rennes1.fr>
2021-03-09 10:48:18 +0100

    App Phase 5

---
+++ src/interface_UI/ResetButton.scala
@@ -0,0 +1,20 @@
+package interface_UI
+import scala.swing._ 
+import event._
+import java.awt.Color
+
+class ResetButton(lab: String, from : InField, to : ResultText, then : Response) extends Button {
+
+  text = lab
+  
+  // Ajout d'une reaction au clic sur le bouton
+  // c'est à dire copier le contenu du champ from dans le label de to.
+  reactions += {
+
+    case ButtonClicked(_) => {
+      from.text = ""
+      to.text = ""
+      then.text=""
+    }
+  }
+}
