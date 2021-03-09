4d4b1ceaf5b0532c8a5a5a2d4e7784f242c66120
lbosse <lbosse@d020m08.istic.univ-rennes1.fr>
2021-03-09 09:06:56 +0100

    update

---
+++ src/Application/test.scala
@@ -0,0 +1,9 @@
+package Application
+import Implementations._
+
+object test extends App{
+  val l= List("théâtre","Rennes","Bretagne")
+  println(BSDImpl.respond(l))
+  println(Tolerance.correct("mairie", "théatre"))
+  println(BSDImpl.hammingDiststr(" ", "de"))
+}
