f1dfd9b30e985584a8d0dc73887315643c8d425c
tboue <tboue@d020m10.istic.univ-rennes1.fr>
2021-03-09 11:55:23 +0100

    App Phase x.alpha3

--- src/interface_UI/ResetButton.scala
+++ src/interface_UI/ResetButton.scala
@@ -6,7 +6,6 @@ import java.awt.Color
 class ResetButton(lab: String, from : InField, to : ScrollPane, then : Response) extends Button {
 
   text = lab
-  val res = new BoxPanel(Orientation.Vertical)
   // Ajout d'une reaction au clic sur le bouton
   // c'est à dire copier le contenu du champ from dans le label de to.
   reactions += {
