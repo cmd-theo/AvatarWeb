8d4c31ddaf373293b78ef42ae3f52a15e3603d47
barkhad <bahdon.barkhad@etudiant.univ-rennes1.fr>
2021-03-13 17:31:13 +0100

    rajout tests dans TestAnalysePhrase

--- tests/machine/TestAnalysePhrase.scala
+++ tests/machine/TestAnalysePhrase.scala
@@ -22,9 +22,21 @@ class TestAnalysePhrase {
   @Test 
   def testRecherche {
     val l:List[String] = List("Coucou","Bonjour","Bonsoir","Bonne nuit","Au revoir","")
-    assertEquals(None,AnalysePhraseImpl.recherche("Je recherche quelque chose", Nil))
-    assertEquals(Some("Bonsoir") , AnalysePhraseImpl.recherche("Bonsoir", l))
-    assertEquals(Some("Bonne nuit") , AnalysePhraseImpl.recherche("Bonnenuit", l))
+    assertEquals(None,AnalysePhraseImpl.recherche("Je recherche quelque chose", Nil)) // liste vide
+    assertEquals(Some(""),AnalysePhraseImpl.recherche("", l)) // liste non vide avec mot correct
+    assertEquals(Some("Bonsoir"),AnalysePhraseImpl.recherche("Bonsoyr", l)) // liste non vide avec mot ayant 1 erreur
+    assertEquals(None,AnalysePhraseImpl.recherche("Bonnenuit ", l)) // liste non vide avec mot ayant + d'1 erreur
+  }
+  
+  @Test
+  def testCompare {
+    val l_keywords:List[List[String]] = List(List("France","Paris","Marseille"),List("Espagne","Madrid","Barcelone"),
+                                   List("Allemagne","Berlin","Hambourg"),List("Royaume-Uni","Londres","Birmingham"),
+                                   List("Italie","Rome","Milan"))
+    assertEquals(None,AnalysePhraseImpl.compare("Liste vide", Nil)) // liste vide
+    assertEquals(Some("Berlin"),AnalysePhraseImpl.compare("Berlin", l_keywords)) // liste non vide avec mot correct
+    assertEquals(Some("Rome"),AnalysePhraseImpl.compare("Romm", l_keywords)) // liste non vide avec mot ayant 1 erreur
+    assertEquals(None,AnalysePhraseImpl.compare("Royaume-nis", l_keywords)) // liste non vide avec mot ayant plusieurs erreurs
   }
   
   @Test
