8c0f4c815285d4d6f516eb0844afe53c25e64a38
Agayeva Gulizar <gulizar.agayeva@etudiant.univ-rennes1.fr>
2021-03-15 21:01:46 +0000

    Update MachineImpl.scala

--- src/Implementations/MachineImpl.scala
+++ src/Implementations/MachineImpl.scala
@@ -20,6 +20,7 @@ object MachineImpl extends MachineDialogue{
   // Pour la partie test par le client
   /**
    * Reinitilisation de l'avatar
+   * Effacement de la conversation en cliquant sur le boutton reset
    */
   def reinit= {
     ResetButton();
