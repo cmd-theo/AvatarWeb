9e850425ac6f6bfbcbb80b08f05bdd819ba098d7
tboulaire <tboulaire@d020m09.istic.univ-rennes1.fr>
2021-03-23 09:04:24 +0100

    reset

--- src/interface_UI/ResetButton.scala
+++ src/interface_UI/ResetButton.scala
@@ -3,16 +3,19 @@ import scala.swing._
 import event._
 import java.awt.Color
 
-class ResetButton(lab: String, from : InField, to : ScrollPane, then : Response) extends Button {
+class ResetButton(lab: String, from : InField, to : ScrollPane, then : SendButton) extends Button {
 
   text = lab
+  
   // Ajout d'une reaction au clic sur le bouton
   // c'est à dire copier le contenu du champ from dans le label de to.
   reactions += {
 
     case ButtonClicked(_) => {
+      val reset = new MainPanel 
       from.text = ""
-      
+      then.temp = reset.p
+      to.contents = reset.main
     }
   }
 }
